0d9bd5832147484d615713b1e94460c61672d9fd
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-22 04:49:20 +0900

    Update watchfont.ts

--- watchfont.ts
+++ watchfont.ts
@@ -40,7 +40,7 @@ namespace watchfont {
      * @param x x座標, eg: 1
      * @param y y座標, eg: 3
      */
-    //% block="消灯 %x %y"
+    //% block="消灯 x|%x x|%y"
     export function unplot(x: number, y: number): void {
         switch (rotate) {
             case 0:
