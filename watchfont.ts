743cae265616472d300b536fd782e61fb427b9a7
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-22 05:31:09 +0900

    Update watchfont.ts

--- watchfont.ts
+++ watchfont.ts
@@ -122,7 +122,7 @@ namespace watchfont {
      * 回転方向を設定する
      * @param r 回転方向
      */
-    //% block="回転方向 %r=左"
+    //% block="回転方向 %r=rotate"
     export function setRotatation(r: rotate): void {
         rotate = r
     }
