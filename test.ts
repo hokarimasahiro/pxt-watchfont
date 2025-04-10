c82d726b0604a1823545b45a3d2fa6e6748a5712
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-10 16:04:02 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,7 +3,12 @@ basic.forever(function () {
         watchfont.plotBarGraph(b)
         basic.pause(10)
     }
-    basic.pause(500);
+    basic.pause(1000);
+    for (let b = 0; b < 100; b++) {
+        watchfont.showNumber2(b)
+        basic.pause(100)
+    }
+    basic.pause(1000);
     for (let b = 0; b < 1024; b++) {
         watchfont.showSorobanNumber(b, 0, 5)
         basic.pause(10)
